9f768cea9c650cc7a656329d0f8aa8b9947918d3
aguty93 <albertogut1993@gmail.com>
2024-02-12 13:16:05 +0100

    IntroBBDD

---
+++ IntroBBDD.sql
@@ -0,0 +1,10 @@
+Ejercicio 1
+SELECT flight_id, flight_no, status FROM flights WHERE status = 'On Time';
+Ejercicio 2
+SELECT * FROM bookings WHERE total_amount >= 1000000;
+Ejercicio 3
+SELECT * FROM aircrafts_data;
+Ejercicio 4
+SELECT flight_id, flight_no FROM flights WHERE aircraft_code = '733';
+Ejercicio 5
+SELECT * FROM tickets WHERE passenger_name LIKE 'IRINA%';
